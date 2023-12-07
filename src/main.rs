8bd97f212169d45177c494be165e14e5a9213bd9
Toufeeq Pasha <toufeeq@sovereignwallet.network>
2023-12-07 22:45:47 +0530

    Added identity column

--- src/main.rs
+++ src/main.rs
@@ -1,50 +1,65 @@
 use anyhow::Result;
 use avail_subxt::{api, build_client, Opts};
-use structopt::StructOpt;
-use sp_arithmetic::Perbill;
+use codec::Encode;
 use prettytable::{row, Table};
+use sp_arithmetic::Perbill;
+use structopt::StructOpt;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-	let start_era = 28;
-	let end_era = 29;
+    let start_era = 29;
+    let end_era = 29;
 
-	let args = Opts::from_args();
-	let client = build_client(args.ws, args.validate_codegen).await?;
+    let args = Opts::from_args();
+    let client = build_client(args.ws, args.validate_codegen).await?;
 
-	for era in start_era..=end_era {
-		let era_points_query = api::storage().staking().eras_reward_points(era);
-		let era_rewards_query = api::storage().staking().eras_validator_reward(era);
-		let era_points = client
-			.storage()
-			.at_latest()
-			.await?
-			.fetch(&era_points_query)
-			.await?
-			.unwrap();
-		let era_rewards = client
-			.storage()
-			.at_latest()
-			.await?
-			.fetch(&era_rewards_query)
-			.await?
-			.unwrap();
-		let era_total_points = era_points.total;
-		let era_validators = era_points.individual;
+    for era in start_era..=end_era {
+        let era_points_query = api::storage().staking().eras_reward_points(era);
+        let era_rewards_query = api::storage().staking().eras_validator_reward(era);
+        let era_points = client
+            .storage()
+            .at_latest()
+            .await?
+            .fetch(&era_points_query)
+            .await?
+            .unwrap();
+        let era_rewards = client
+            .storage()
+            .at_latest()
+            .await?
+            .fetch(&era_rewards_query)
+            .await?
+            .unwrap();
+        let era_total_points = era_points.total;
+        let era_validators = era_points.individual;
 
-		println!(
-			"Era: {}, total_points: {}, total_rewards: {}",
-			era, era_total_points, era_rewards
-		);
+        println!(
+            "Era: {}, total_points: {}, total_rewards: {}",
+            era, era_total_points, era_rewards
+        );
 
-		// Create the table
+        // Create the table
         let mut table = Table::new();
         table.set_format(*prettytable::format::consts::FORMAT_NO_BORDER_LINE_SEPARATOR);
 
         // Add table header
-        table.add_row(row!["Validator AccountId", "Reward Points", "Rewards", "Rewards %"]);
+        table.add_row(row![
+            "Validator AccountId",
+            "Identity",
+            "Reward Points",
+            "Rewards",
+            "Rewards %"
+        ]);
 
         for (validator_account_id, reward_points) in era_validators {
+            let id_query = api::storage().identity().identity_of(&validator_account_id);
+            let id = client.storage().at_latest().await?.fetch(&id_query).await?;
+
+            let id_bytes = match id {
+                Some(id) => id.info.display.encode()[1..].to_vec(),
+                None => vec![],
+            };
+            let validator_id = String::from_utf8(id_bytes).unwrap_or_default();
             // Calculate rewards percent
             let rewards_percent = Perbill::from_rational(reward_points, era_total_points);
 
@@ -54,6 +69,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
             // Add row to the table
             table.add_row(row![
                 validator_account_id,
+                &validator_id,
                 reward_points,
                 rewards,
                 format!("{:#?}", rewards_percent)
@@ -62,6 +78,6 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
         // Print the table
         table.printstd();
-	}
-	Ok(())
+    }
+    Ok(())
 }
