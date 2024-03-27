32b5d3f0723d40fa1c3d3aee9f73290d7ee96e8e
Toufeeq Pasha <toufeeqpasha.2014@gmail.com>
2024-03-27 12:56:09 +0530

    Updated to use avail-subxt-v0.5 (subxt-v0.34)

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
 use anyhow::Result;
 pub mod account_utils;
 pub mod batch_transfer;
-mod create_pools;
-mod periodic_da_submit;
-mod scale_encode;
-mod staking_utils;
-mod validator_rewards;
+// mod create_pools;
+// mod periodic_da_submit;
+// mod scale_encode;
+// mod staking_utils;
+// mod validator_rewards;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -13,7 +13,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     // batch_transfer::batch_transfer(10_000_000_000_000_000_000u64).await?;
     // staking_utils::bond_and_nominate_batch(9_000_000_000_000_000_000u64).await?;
     let execution_start = std::time::Instant::now();
-    validator_rewards::fetch_blocks(256).await?;
+    batch_transfer::batch_transfer(10_000_000_000_000_000_000_000u128).await?;
     // periodic_da_submit::submit_txs().await?;
     println!("Time elapsed: {:?}", execution_start.elapsed());
     Ok(())
