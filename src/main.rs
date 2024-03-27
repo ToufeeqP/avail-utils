8886ea7afd33c635ae7467b67133833e5b317e0e
Toufeeq Pasha <toufeeqpasha.2014@gmail.com>
2024-03-27 14:32:06 +0530

    updated da_submit util

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use anyhow::Result;
 pub mod account_utils;
 pub mod batch_transfer;
 mod create_pools;
-// mod periodic_da_submit;
+mod periodic_da_submit;
 // mod scale_encode;
 // mod staking_utils;
 // mod validator_rewards;
@@ -13,8 +13,8 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     // batch_transfer::batch_transfer(10_000_000_000_000_000_000u64).await?;
     // staking_utils::bond_and_nominate_batch(9_000_000_000_000_000_000u64).await?;
     let execution_start = std::time::Instant::now();
-    batch_transfer::batch_transfer(10_000_000_000_000_000_000_000u128).await?;
-    // periodic_da_submit::submit_txs().await?;
+    // batch_transfer::batch_transfer(10_000_000_000_000_000_000_000u128).await?;
+    periodic_da_submit::submit_txs().await?;
     println!("Time elapsed: {:?}", execution_start.elapsed());
     Ok(())
 }
--- src/periodic_da_submit.rs
+++ src/periodic_da_submit.rs
@@ -1,9 +1,8 @@
-use avail_subxt::{avail, build_client, submit_data, AvailConfig, Opts};
+#![allow(dead_code)]
+use avail_subxt::{submit::submit_data_with_nonce, AvailClient, Opts};
+use sp_arithmetic::traits::One;
 use structopt::StructOpt;
-use subxt::{
-    ext::sp_core::{sr25519::Pair, Pair as _},
-    tx::PairSigner,
-};
+use subxt_signer::sr25519::dev;
 use tokio::time::Duration;
 
 // Note: padded_block_length is the limiting factor for AVAIL chain, even though BlockLength may be 5MB or higher, padded_block_length needs to be considered to check how much length a block can have.
@@ -17,24 +16,25 @@ const ACCT_SEED: &str =
 
 pub async fn submit_txs() -> anyhow::Result<()> {
     let args = Opts::from_args();
-    let (client, _) = build_client(args.ws, args.validate_codegen).await?;
+    let client = AvailClient::new(args.ws).await?;
 
     println!(
         "{} txs of size {} will be submitted in every {} seconds",
         NUM_CHUNKS, TX_SIZE, DELAY_SECS
     );
-    let pair_a = Pair::from_string_with_seed(ACCT_SEED, None)?;
-    let signer = PairSigner::<AvailConfig, avail::Pair>::new(pair_a.0);
-
+    // let mnemonic = Mnemonic::parse(ACCT_SEED)?;
+    // let signer = Keypair::from_phrase(&mnemonic, None)?;
+    let signer = dev::alice();
+    let signer_id = signer.public_key().into();
+    let mut nonce = client.tx().account_nonce(&signer_id).await?;
     loop {
         for i in 1..=NUM_CHUNKS {
             let data = vec![(i & 255) as u8; TX_SIZE];
-            let _h = submit_data(&client, &signer, data, 1).await?;
-            // println!("hash #{i}: {:?}", h);
+            let _ = submit_data_with_nonce(&client, &signer, data, 1, nonce).await?;
+            nonce = nonce.saturating_add(One::one());
         }
 
         // Add a delay between iterations
         tokio::time::sleep(Duration::from_secs(DELAY_SECS)).await;
     }
-
 }
