739411e7e27c1c3ecec2a22043c8396b550aaeba
Toufeeq Pasha <toufeeqpasha.2014@gmail.com>
2024-03-27 15:04:28 +0530

    updated staking utils

--- src/main.rs
+++ src/main.rs
@@ -4,18 +4,17 @@ pub mod batch_transfer;
 mod create_pools;
 mod periodic_da_submit;
 mod scale_encode;
-// mod staking_utils;
+mod staking_utils;
 // mod validator_rewards;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     // account_utils::generate_accounts(10)?;
-    // batch_transfer::batch_transfer(10_000_000_000_000_000_000u64).await?;
-    // staking_utils::bond_and_nominate_batch(9_000_000_000_000_000_000u64).await?;
     let execution_start = std::time::Instant::now();
     // batch_transfer::batch_transfer(10_000_000_000_000_000_000_000u128).await?;
+    staking_utils::bond_and_nominate_batch(1_000_000_000_000_000_000_000u128).await?;
     // periodic_da_submit::submit_txs().await?;
-    scale_encode::encode();
+    // scale_encode::encode();
     println!("Time elapsed: {:?}", execution_start.elapsed());
     Ok(())
 }
--- src/staking_utils.rs
+++ src/staking_utils.rs
@@ -2,28 +2,23 @@
 use crate::account_utils::{Account, ACCOUNT_PATH};
 use anyhow::Result;
 use avail_subxt::{
-    api::{self, runtime_types::pallet_staking::pallet::pallet::Call as StakingCall},
-    build_client,
-    primitives::AvailExtrinsicParams,
+    api::{self, runtime_types::pallet_staking::pallet::pallet::Call as StakingCall, Call},
+    primitives::new_params_from_app_id,
     utils::{AccountId32, MultiAddress},
-    AvailConfig, Call, Opts,
+    AvailClient, Opts,
 };
 use serde::{Deserialize, Serialize};
 use sp_arithmetic::traits::SaturatedConversion;
-use std::fs;
-use std::str::FromStr;
+use std::{fs, str::FromStr};
 use structopt::StructOpt;
-use subxt::{
-    ext::sp_core::{sr25519::Pair, Pair as _},
-    tx::PairSigner,
-};
+use subxt_signer::{bip39::Mnemonic, sr25519::Keypair};
 
 #[derive(Serialize, Deserialize)]
 struct Validator {
     address: String,
 }
 
-pub async fn bond_and_nominate_batch(amount: u64) -> Result<()> {
+pub async fn bond_and_nominate_batch(amount: u128) -> Result<()> {
     // Read accounts from the accounts.json file
     let accounts_json = fs::read_to_string(ACCOUNT_PATH)?;
     let accounts: Vec<Account> = serde_json::from_str(&accounts_json)?;
@@ -33,13 +28,13 @@ pub async fn bond_and_nominate_batch(amount: u64) -> Result<()> {
     let validators: Vec<Validator> = serde_json::from_str(&validators_json)?;
 
     let args = Opts::from_args();
-    let (client, _) = build_client(args.ws, false).await?;
-
-    let extrinsic_params = AvailExtrinsicParams::new_with_app_id(0.into());
+    let client = AvailClient::new(args.ws).await?;
 
     for (account, validator) in accounts.iter().zip(validators.iter()) {
-        let pair = Pair::from_string_with_seed(&account.mnemonic, None)?;
-        let signer = PairSigner::<AvailConfig, Pair>::new(pair.0);
+        let extrinsic_params = new_params_from_app_id(0);
+
+        let mnemonic = Mnemonic::parse(&account.mnemonic)?;
+        let signer = Keypair::from_phrase(&mnemonic, None)?;
 
         let validator_id = AccountId32::from_str(&validator.address)?;
 
@@ -58,7 +53,7 @@ pub async fn bond_and_nominate_batch(amount: u64) -> Result<()> {
         let batch_tx = api::tx().utility().batch(vec![bond_call, nominate_call]);
         let h = client
             .tx()
-            .sign_and_submit(&batch_tx, &signer, extrinsic_params.clone())
+            .sign_and_submit(&batch_tx, &signer, extrinsic_params)
             .await?;
         println!("Batch operation completed with hash: {:?}", h);
     }
