537961eb5a6505f5490f41e078a3afdff6fa0773
Toufeeq Pasha <toufeeq@sovereignwallet.network>
2024-05-27 12:04:22 +0530

    Added block seal verification

--- src/block_author.rs
+++ src/block_author.rs
@@ -1,13 +1,20 @@
 #![allow(dead_code)]
 use anyhow::Result;
+use avail_core::header::Header as DaHeader;
 use avail_subxt::{
-    api, api::runtime_types::sp_consensus_babe::digests::PreDigest, AvailClient, Opts,
+    api::{self, runtime_types::sp_consensus_babe::digests::PreDigest},
+    AvailClient, Opts,
 };
 use codec::{Decode, Encode};
-use sp_core::H256;
-use sp_runtime::RuntimeDebug;
+use hex_literal::hex;
+use sp_consensus_babe::AuthorityId;
+use sp_core::{ByteArray, H256};
+use sp_runtime::{traits::Header, RuntimeDebug};
 use structopt::StructOpt;
 use subxt::config::substrate::DigestItem;
+
+pub type BlockNumber = u32;
+pub type HeaderT = DaHeader<BlockNumber, sp_runtime::traits::BlakeTwo256>;
 
 /// The `ConsensusEngineId` of BABE.
 pub const BABE_ENGINE_ID: [u8; 4] = *b"BABE";
@@ -35,8 +42,7 @@ pub async fn find_author(block_id: Option<BlockId>) -> Result<()> {
                     .chain_get_block_hash(Some(block_number.into()))
                     .await?
                     .expect("header exist");
-                let block = client.blocks().at(block_hash).await?;
-                block
+                client.blocks().at(block_hash).await?
             }
         },
     };
@@ -47,9 +53,8 @@ pub async fn find_author(block_id: Option<BlockId>) -> Result<()> {
     for digest in digests {
         if let DigestItem::PreRuntime(id, data) = digest {
             if id == BABE_ENGINE_ID {
-                let pre_digest: PreDigest = PreDigest::decode(&mut data.as_ref())
-                    .ok()
-                    .expect("Lets see");
+                let pre_digest: PreDigest =
+                    PreDigest::decode(&mut data.as_ref()).expect("Lets see");
                 let auth_index = authority_index(pre_digest);
                 // fetch the validator list
                 let vals_query = api::storage().session().validators();
@@ -61,11 +66,9 @@ pub async fn find_author(block_id: Option<BlockId>) -> Result<()> {
                     .unwrap();
                 let validator = validators.get(auth_index as usize);
                 match validator {
-                    Some(validator) => println!(
-                        "author of block #{} is {}",
-                        header.number,
-                        validator.to_string()
-                    ),
+                    Some(validator) => {
+                        println!("author of block #{} is {}", header.number, validator)
+                    }
                     None => println!("No author for block #{}", header.number,),
                 }
             }
@@ -80,4 +83,49 @@ fn authority_index(digest: PreDigest) -> u32 {
         PreDigest::SecondaryPlain(secondary) => secondary.authority_index,
         PreDigest::SecondaryVRF(secondary) => secondary.authority_index,
     }
+}
+
+pub async fn verify_seal(block_id: Option<BlockId>) -> Result<()> {
+    use sp_consensus_babe::digests::*;
+    use sp_runtime::RuntimeAppPublic;
+
+    let args = Opts::from_args();
+    let client = AvailClient::new(args.ws).await?;
+
+    let block = match block_id {
+        None => client.blocks().at_latest().await?,
+        Some(id) => match id {
+            BlockId::Hash(block_hash) => client.blocks().at(block_hash).await?,
+            BlockId::Number(block_number) => {
+                let block_hash = client
+                    .legacy_rpc()
+                    .chain_get_block_hash(Some(block_number.into()))
+                    .await?
+                    .expect("header exist");
+                client.blocks().at(block_hash).await?
+            }
+        },
+    };
+
+    let header = block.header();
+    let verify_seal_signature = |mut header: HeaderT, signer: &AuthorityId| {
+        let seal = header.digest_mut().pop()?.as_babe_seal()?;
+        let pre_hash = header.hash();
+
+        if !signer.verify(&pre_hash.as_ref(), &seal) {
+            return None;
+        }
+
+        Some(())
+    };
+
+    let local_header = HeaderT::decode(&mut header.encode().as_ref()).unwrap();
+
+    // creating public key from bytes
+    let key_bytes = hex!("5eb6d4f14715a25cbb43415a559ffcf9a197e6875315d9ef438b9bbdcec9ee6b");
+    let signer_key =
+        AuthorityId::from_slice(&key_bytes).expect("Failed to get public key froom bytes");
+    verify_seal_signature(local_header, &signer_key).unwrap();
+    println!("Seal verification successful");
+    Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
+#![allow(dead_code)]
 use anyhow::Result;
+use serde::{Deserialize, Serialize};
 pub mod account_utils;
 pub mod batch_transfer;
 mod create_pools;
@@ -13,7 +15,9 @@ mod validator_rewards;
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     // account_utils::generate_accounts(10)?;
     let execution_start = std::time::Instant::now();
-    block_author::find_author(Some(block_author::BlockId::Number(8497))).await?;
+    // check_block_authors().await?;
+    block_author::verify_seal(Some(block_author::BlockId::Number(8497))).await?;
+    // block_author::find_author(Some(block_author::BlockId::Number(8497))).await?;
     // scale_encode::decode_justification();
     // db_utils::run();
     // csv_accounts::generate_accounts(7500)?;
@@ -34,5 +38,94 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     // periodic_da_submit::submit_txs().await?;
     // scale_encode::encode();
     println!("Time elapsed: {:?}", execution_start.elapsed());
+    Ok(())
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct BlockIds {
+    nodes: Vec<u32>,
+}
+
+impl BlockIds {
+    pub fn from_json(slice: &[u8]) -> Result<Self, serde_json::Error> {
+        serde_json::from_slice(slice)
+    }
+}
+
+async fn check_block_authors() -> Result<()> {
+    // turing
+    // let blocks = r#"{
+    //     "nodes": [
+    //                 4177,
+    //                 8497,
+    //                 9773,
+    //                 14092,
+    //                 18411,
+    //                 22730,
+    //                 27050,
+    //                 28358,
+    //                 32677,
+    //                 36997,
+    //                 41317,
+    //                 45636,
+    //                 49954,
+    //                 54274,
+    //                 58455,
+    //                 62775,
+    //                 67065,
+    //                 71268,
+    //                 75588,
+    //                 79855,
+    //                 84175,
+    //                 88369,
+    //                 92630,
+    //                 96934,
+    //                 101231,
+    //                 105515,
+    //                 109832,
+    //                 114055,
+    //                 118371,
+    //                 122683,
+    //                 127003,
+    //                 131319,
+    //                 135614,
+    //                 139889,
+    //                 144167,
+    //                 148455,
+    //                 152748,
+    //                 157048,
+    //                 161302,
+    //                 165620,
+    //                 169939,
+    //                 174257,
+    //                 178568,
+    //                 182869,
+    //                 187173,
+    //                 191475,
+    //                 195795,
+    //                 200115,
+    //                 204411,
+    //                 208727,
+    //                 213046,
+    //                 217350,
+    //                 221664,
+    //                 225958,
+    //                 230277,
+    //                 234584
+    //     ]
+    // }"#;
+
+    // hex
+    let blocks = r#"{
+        "nodes": [
+          90691, 8641, 86373, 82057, 77739, 73421, 69102, 64787, 60469, 56150, 51830,
+          47511, 4321, 43194, 38876, 34558, 30239, 25919, 21601, 17281, 12961
+        ]
+      }
+      "#;
+    let block_ids = BlockIds::from_json(blocks.as_bytes()).unwrap();
+    for node in block_ids.nodes {
+        block_author::find_author(Some(block_author::BlockId::Number(node))).await?;
+    }
     Ok(())
 }
